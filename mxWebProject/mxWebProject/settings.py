4e3cb7a93a26df2a37e1ceb26f9b83e72d521f97
hijj <11391786942qq.com>
2019-02-13 08:55:36 +0800

    添加数据库配置-第二次

--- mxWebProject/mxWebProject/settings.py
+++ mxWebProject/mxWebProject/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'mxWebProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mxWeb',
+        'USER':'ROOT',
+        'PASSWORD':'',
+        'HOST':'127.0.0.1',
     }
 }
 
