b4f778bb22c088df35a342761ab114ad05c3fe05
hijj <11391786942qq.com>
2019-02-13 19:50:35 +0800

    增加了course和orangazition

--- mxWebProject/users/models.py
+++ mxWebProject/users/models.py
@@ -1,5 +1,6 @@
 #_*_encoding:utf-9_*_
 from __future__ import unicode_literals
+from datetime import datetime
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
@@ -19,11 +20,32 @@ class UserProfile(AbstractUser):
     def __unicode__(self):
         return self.username
 
+class EmailVerifyRecord(models.Model):
+    code = models.CharField(max_length=20,verbose_name=u"验证码")
+    email = models.EmailField(max_length=50,verbose_name=u"邮箱")
+    send_type=models.CharField(verbose_name=u'验证码类型',choices=( ("register",u"注册"),
+                                  ("forget",u"找回密码"),("update_email",u"修改邮箱") ),
+                                max_length=30)
+    send_time=models.DateTimeField(verbose_name=u"发送时间",default=datetime.now)
+
+    class Meta:
+        verbose_name=u"邮箱验证码"
+        verbose_name_plural=verbose_name
+
+    def __unicode__(self):
+        return "{0}({1})".format(self.code,self.email)
 
 
+class Ban(models.Model):
+    title=models.CharField(max_length=100,verbose_name=u"标题")
+    image=models.ImageField(max_length=100,upload_to="banner/%Y/%m",verbose_name=u"轮播图")
+    utl=models.URLField(max_length200,verbose_name=u"访问地址")
+    index=models.IntegerField(default=100,verbose_name=u"播放顺序")
+    add_time=models.DateTimeField(dafualt=datetime.now,verbose_name=u"添加时间")
 
-
-
+    class Meta:
+        verbose_name = u"轮播图"
+        verbose_name_plural = verbose_name
 
 
 
